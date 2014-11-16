0c0759dca101986190fcc2c984e98e0a61427e7d
Nick Giancola <nick@gophilosophie.com>
2014-11-16 15:01:48 -0800

    Fix pagination. Octokits auto_paginate is a lie.

--- github_search_workflow/fetcher.rb
+++ github_search_workflow/fetcher.rb
@@ -5,16 +5,26 @@ module GithubSearchWorkflow
       require 'netrc'
 
       client = Octokit::Client.new(netrc: true)
-      client.auto_paginate = true
 
-      user_repos = client.repos
+      user_repos = paginate client.repos, client
 
       user_orgs = client.orgs
-      org_repos = user_orgs.map { |o| o.rels[:repos].get.data }.flatten
+      org_repos = user_orgs
+        .map { |o| paginate(client.get(o.rels[:repos].href), client) }.flatten
 
       repos = (user_repos + org_repos).map { |r| Repo.from_github(r) }
 
       Cache.write(repos.map(&:to_cache).to_json)
+    end
+
+    private
+
+    def paginate(data, client)
+      if next_page = client.last_response.rels[:next]
+        paginate((data + client.get(next_page.href)), client)
+      else
+        data
+      end
     end
   end
 end
